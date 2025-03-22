0660f631bd9b00078d847bdd53c6f3b9036455d6
mattia-mura <mattia.murador21@gmail.com>
2025-03-22 22:50:32 +0100

    Add files via upload

---
+++ Menedor2TPSIT/menedor2bank/src/main/java/classi/Investimenti.java
@@ -0,0 +1,25 @@
+package classi;
+
+public class Investimenti {
+
+    private int durata;
+    private double guadagno;
+
+
+    public Investimenti(int durata,double guadagno){
+        this.durata=durata;
+        this.guadagno=guadagno;
+    }
+
+    public void scalaTempo (){
+        this.durata = this.durata-1;
+    }
+
+    public int getTempo(){
+        return this.durata;
+    }
+
+    public double getGuadagno(){
+        return this.guadagno;
+    }
+}
